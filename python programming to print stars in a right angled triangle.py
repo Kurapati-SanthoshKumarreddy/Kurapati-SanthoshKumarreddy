8acdef07307616683cbe1916a20dfff9471308ee
Kurapati Santhosh Kumar Reddy <109294580+Kurapati-SanthoshKumarreddy@users.noreply.github.com>
2022-07-25 07:30:26 +0530

    Add files via upload

---
+++ python programming to print stars in a right angled triangle.py
@@ -0,0 +1,8 @@
+n=int(input("enter the number of rows"))
+o=int(input("enter the number of columns"))
+for i in range(6):
+    for j in range(6):
+        print("*",end=" ")
+    print("*",end=" ")
+    print()
+    
